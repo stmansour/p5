e037ebff0df525b110343135b1f1c5d83bb817d2
Steve Mansour <sman@stevemansour.com>
2022-08-22 12:11:50 -0700

    added first genetics code

---
+++ examples/genetics1/dna.js
@@ -0,0 +1,66 @@
+/*jshint esversion: 6 */
+
+//  A trivial DNA class that supports:
+//    * determining its fitness  (THIS BELONGS SOMEWHERE ELSE)
+//    * crossover
+//    * mutation
+//============================================================================
+class DNA {
+    constructor(id) {
+        this.fitness = 0;
+        this.genes = '';
+        if (typeof id === "undefined") {
+            this.id = 0;
+            this.genes = [];
+        } else {
+            this.id = id; // should be unique across all the DNA instances
+            for (var i = 0; i < app.phrase.length; i++) {
+                this.genes += String.fromCharCode(this.randomChar());
+            }
+        }
+    }
+
+    randomChar() {
+        return floor(random(32,127));
+    }
+
+    determineFitness() {
+        //  how many characters match the target phrase
+        let score = 0;
+        for (let i = 0; i < this.genes.length; i++) {
+            if (this.genes[i] == app.phrase[i]) {
+                score++;
+            }
+        }
+        this.fitness = score/app.phrase.length;
+    }
+
+    crossover(d) {
+        let child = new DNA(); // we know its id is not unique, we'll name it later
+        let gl = this.genes.length;
+        // let m = floor(random(gl));
+        for (let i = 0; i < gl; i++) {
+            if (random() > 0.5) {
+                child.genes += this.genes[i];
+            } else {
+                child.genes += d.genes[i];
+            }
+        }
+        return child;
+    }
+
+
+    mutate() {
+        for (let i = 0; i < this.genes.length; i++) {
+            if (random() < app.mutationRate) {
+                let rc = this.randomChar();
+                let s = this.genes.substring(0,i) + String.fromCharCode(rc);
+                if (i < this.genes.length-1) {
+                    s += this.genes.substring(i+1);
+                }
+                this.genes = s;
+            }
+        }
+    }
+
+}
---
+++ examples/genetics1/main.js
@@ -0,0 +1,145 @@
+/*jshint esversion: 6 */
+var app = {
+    width: 800,
+    height: 400,
+    phrase: "Approaching like a drowning wall of love",
+    population: [],
+    populationSize: 500,
+    generations: 0,
+    mutationRate: 0.009,
+    bestDNA: "",
+    bestFitness: 0,
+};
+
+function newPopulation() {
+    let p = [];
+    for (var i = 0; i < app.populationSize; i++) {
+        p.push(new DNA(i));
+    }
+    app.population = p;
+}
+
+function setup() {
+    newPopulation();
+}
+
+function draw() {
+    showPopulation();  // show what we got
+
+    // compute fitness
+    let j = 0;
+    for (let i = 0; i < app.population.length; i++) {
+        app.population[i].determineFitness();
+        if (app.population[i].fitness > app.population[j].fitness) {
+            j = i;
+        }
+    }
+    app.bestDNA = app.population[j].genes;
+    app.bestFitness = app.population[j].fitness;
+    updateUI();
+    if (app.bestFitness > 0.999999) {
+        app.population.sort((a,b) => {
+            if (a.fitness > b.fitness) {
+                return -1;
+            } else if (a.fitness < b.fitness) {
+                return 1;
+            }
+            return 0;
+        });
+        showPopulation();
+        noLoop();
+        return;
+    }
+
+    // create a mating pool
+    let matingPool = [];
+    for (let i = 0; i < app.population.length; i++) {
+        let n = floor(app.population[i].fitness * 100);  // n is now an integer between 0 and 100
+        for (let j = 0; j < n; j++) {
+            matingPool.push(app.population[i]);
+        }
+    }
+
+    // create next generation:
+    //     pick two parents at random, have them produce and offspring
+    let newpop = [];
+    for (let i = 0; i < app.population.length; i++) {
+        let parents = chooseParents(matingPool);
+        let kid = parents[0].crossover(parents[1]);
+        kid.mutate();
+        kid.id = i; // crossover does not name the kid
+        newpop.push(kid);
+    }
+    app.population = newpop;
+    app.generations += 1;
+
+}
+
+
+function chooseParents(p) {
+    let mom = null;
+    let dad = null;
+    do {
+        mom = p[floor(random(p.length))];
+        dad = p[floor(random(p.length))];
+    } while (mom.id == dad.id);  // if we happened to pick the same parents, try again
+    return [mom, dad];
+}
+
+function showPopulation() {
+    let s = "";
+    for (let i = 0; i < 50; i++) {
+        s += HtmlEncode(app.population[i].genes) + "<br>";
+    }
+    setInnerHTML("populationData",s);
+}
+
+function HtmlEncode(s) {
+  var el = document.createElement("div");
+  el.innerText = el.textContent = s;
+  s = el.innerHTML;
+  return s;
+}
+
+function setInnerHTML(id,s) {
+    let el = document.getElementById(id);
+    if (el != null) {
+        el.innerHTML = s;
+    }
+}
+
+function initUI() {
+    // app.animationFrameRateSlider = createSlider(2,60,app.animationFrameRate,1);
+    // app.animationFrameRateSlider.parent('animationFrameRateSlider');
+    //
+    // app.orderNSlider = createSlider(1,7,app.orderN,1);
+    // app.orderNSlider.parent('orderNSlider');
+    //
+    // app.showCirclesCheckbox = createCheckbox("Show Circles", app.showCircles);
+    // app.showCirclesCheckbox.parent('showCirclesCheckbox');
+}
+
+function updateUI() {
+    setInnerHTML("targetPhrase", app.phrase);
+    setInnerHTML("initialPopulation", app.populationSize);
+    setInnerHTML("generations", app.generations);
+    setInnerHTML("bestDNA", app.bestDNA);
+    setInnerHTML("mutationRate", ((app.mutationRate * 100).toFixed(2)) + "%" );
+    setInnerHTML("bestFitness",app.bestFitness);
+
+    // app.showCircles = app.showCirclesCheckbox.checked();
+    // // setInnerHTML("showCircles", "" + app.showCircles);
+}
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) {
+        return str;
+    }
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
+
+function checkUndefined(s) {
+    if (s.includes("undefined")) {
+        console.log("found undefined in: " + s);
+    }
+}
